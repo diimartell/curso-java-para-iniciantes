0c3f5aefb4c228b27ea03cc10e767258a97eb3bd
diimartell <ingridemcsilva@gmail.com>
2025-05-02 00:56:01 -0300

    feat: add anotações de Estrutura de Controle

--- ESTRUTURA_CONTROLE/Controle.java
+++ ESTRUTURA_CONTROLE/Controle.java
@@ -3,10 +3,38 @@ package ESTRUTURA_CONTROLE;
 public class Controle {
     public static void main(String[] args) {
         
-        //Estruturas de Condicionais em Java
+        /* Estruturas de Condicionais em Java
 
-        //Declaração de variáveis
+        Ex. Determinando se uma pessoa é maior ou menor de idade: */
+
+        int idade = 23;
+
+        if (idade >= 18) {
+            System.out.println("Você é maior de idade.");
+        } else {
+            System.out.println("Você é menor de idade.");
+        }   
         
+        //Ex. Determinando se um aluno está aprovado ou reprovado:
 
+        double nota = 6.3;
+        if (nota >= 7.0) {
+            System.out.println("Aluno aprovado.");
+        } else {
+            System.out.println("Aluno reprovado.");
+        }
+
+        /* Usando o comparador "else if"
+
+        Ex. Verificando a hora do dia e imprimindo uma mensagem de saudação: */
+
+        int hora = 10;
+        if (hora < 12) {
+            System.out.println("Bom dia!");
+        } else if (hora < 18) {
+            System.out.println("Boa tarde!");
+        } else {
+            System.out.println("Boa noite!");
+        }
     }
 }
